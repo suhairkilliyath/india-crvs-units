18acd834cb1836b8ecdfbad3d78178d4f8c6f5a0
suhairkilliyath <suhairkilliyath@gmail.com>
2023-04-14 05:15:58 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,125 +1,21 @@
-let users = [];
+const loginBtn = document.querySelector('.login-btn');
+const loginForm = document.querySelector('.login-form');
 
-function registerUser(email, password) {
-  if (users.some(user => user.email === email)) {
-    return [false, "Email already exists"];
-  }
-  
-  users.push({ email: email, password: password });
-  return [true, "Registration successful"];
-}
+loginBtn.addEventListener('click', () => {
+  loginForm.classList.toggle('hidden');
+});
 
-function getPassword() {
-  let passwordCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-  let password = "";
-  for (let i = 0; i < 12; i++) {
-    password += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-  }
-  return password;
-}
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const submitBtn = document.querySelector('.submit-btn');
 
-function loginUser(email, password) {
-  let user = users.find(user => user.email === email);
-  if (!user) {
-    return [false, "Email does not exist"];
-  } else if (user.password !== password) {
-    return [false, "Incorrect password"];
+submitBtn.addEventListener('click', () => {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  if (email === "" || password === "") {
+    alert("Please enter both email and password.");
   } else {
-    return [true, "Login successful"];
+    // perform login operation
+    alert(`Login successful for user with email: ${email}`);
   }
-}
-
-function renderRegisterPage() {
-  let email = "";
-  let password = "";
-  
-  document.querySelector("#content").innerHTML = `
-    <h1>User Registration</h1>
-    <div>
-      <label for="email">Email:</label>
-      <input type="text" id="email" value="${email}">
-    </div>
-    <div>
-      <label for="password">Password:</label>
-      <input type="password" id="password" value="${password}">
-    </div>
-    <div>
-      <button id="register-button">Register</button>
-    </div>
-  `;
-  
-  document.querySelector("#register-button").addEventListener("click", () => {
-    email = document.querySelector("#email").value;
-    password = document.querySelector("#password").value;
-    
-    if (email === "" || password === "") {
-      alert("Please enter both email and password");
-      return;
-    }
-    
-    let [success, message] = registerUser(email, password);
-    if (success) {
-      alert(message);
-    } else {
-      alert(message);
-    }
-  });
-}
-
-function renderLoginPage() {
-  let email = "";
-  let password = "";
-  
-  document.querySelector("#content").innerHTML = `
-    <h1>User Login</h1>
-    <div>
-      <label for="email">Email:</label>
-      <input type="text" id="email" value="${email}">
-    </div>
-    <div>
-      <label for="password">Password:</label>
-      <input type="password" id="password" value="${password}">
-    </div>
-    <div>
-      <button id="login-button">Login</button>
-    </div>
-  `;
-  
-  document.querySelector("#login-button").addEventListener("click", () => {
-    email = document.querySelector("#email").value;
-    password = document.querySelector("#password").value;
-    
-    if (email === "" || password === "") {
-      alert("Please enter both email and password");
-      return;
-    }
-    
-    let [success, message] = loginUser(email, password);
-    if (success) {
-      alert(message);
-    } else {
-      alert(message);
-    }
-  });
-}
-
-function renderHomePage() {
-  let email = localStorage.getItem("email");
-  
-  document.querySelector("#content").innerHTML = `
-    <h1>Welcome ${email}!</h1>
-    <div>
-      <button id="logout-button">Logout</button>
-    </div>
-  `;
-  
-  document.querySelector("#logout-button").addEventListener("click", () => {
-    localStorage.removeItem("email");
-    renderLoginPage();
-  });
-}
-
-function renderPage() {
-  let page = localStorage.getItem("page");
- 
-}
+});
